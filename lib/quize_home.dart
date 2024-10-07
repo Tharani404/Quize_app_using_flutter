aec2edb1081f442e61f7d2cd1ba34302017d34e7
Tharani404 <164305986+Tharani404@users.noreply.github.com>
2024-10-07 22:00:07 +0530

    Add a text and change the code

--- lib/quize_home.dart
+++ lib/quize_home.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
-class QuizeHome extends StatefulWidget {
+class QuizeHome extends StatelessWidget {
   const QuizeHome({super.key});
 
   @override
-  State<QuizeHome> createState() => _QuizeHomeState();
-}
-
-class _QuizeHomeState extends State<QuizeHome> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  build(context) {
+    return const Center(child:  Text('Start Screen'),
+    );
   }
 }
