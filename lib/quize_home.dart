af2e75a1088d85d62d3b7d7ecb4409a260f636dc
Tharani404 <164305986+Tharani404@users.noreply.github.com>
2024-10-09 13:34:57 +0530

    Create a widget and add image, text and button

--- lib/quize_home.dart
+++ lib/quize_home.dart
@@ -5,7 +5,37 @@ class QuizeHome extends StatelessWidget {
 
   @override
   build(context) {
-    return const Center(child:  Text('Start Screen'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+          width: 200,
+          ),
+          
+          const SizedBox(
+            height: 30,
+          ),
+
+          const Text('Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 250, 250),
+              fontSize: 20,
+            ),
+          ),
+
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {}, 
+
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+
+            child: const Text('Start Quiz'),
+          )
+        ],
+      ),
     );
   }
 }
